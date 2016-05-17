5849e29085157bf142a45596cdf75014ca39204b
Kevin van Dijk <kevin@edgybits.com>
2016-05-17 02:28:49 +0200

    Create intent route
    Includes only playMedia for now

--- src/router.js
+++ src/router.js
@@ -2,7 +2,23 @@ import router from 'koa-router';
 import { playMediaRequest, SAY } from './actions';
 
 export default function (state, dispatch) {
-  return (helpers) => (
+  async function intent(ctx, waitForAction) {
+    const { body } = ctx.request;
+
+    if (body.data.type === 'playMedia') {
+      dispatch(playMediaRequest(body.data.title));
+    }
+
+    if (body.data.waitForReply) {
+      const result = await waitForAction(SAY);
+      ctx.body = result.payload;
+    } else {
+      ctx.status = 202;
+    }
+  }
+
+  return ({ waitForAction }) => (
     router()
+      .post('/intent', async (ctx) => intent(ctx, waitForAction))
   );
 }
